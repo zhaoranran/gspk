a1282cca8d32431ca87e01c1b313cc9a120e1d4f
zhaoranran_qm <1239668660@qq.com>
2021-04-30 14:51:19 +0800

    添加页面

---
+++ config.js
@@ -0,0 +1,17 @@
+const baseUrl = 'https://free.liaoningdoupo.com';
+
+module.exports = {
+  service: {
+    appId: 'wx93f5346ce47f3cd6',
+    loginUrl: `${baseUrl}/auth/`, // 根据微信ID查询用户信息
+    deptList: `${baseUrl}/hqbi/dept/all`, // 查询部门信息、get方法
+    saveUser: `${baseUrl}/hqbi/user`, // 添加用户信息、post提交
+    userSign: `${baseUrl}/hqbi/user/usersign`, // 用户签到、post方法
+    userAll: `${baseUrl}/hqbi/user/all`, // 查询用户排行榜、get方法
+    userScore: `${baseUrl}/hqbi/user/score`, // 更新用户积分、post方法
+    findQue: `${baseUrl}/hqbi/que/find`, // 随机抽取一定条数题、get方法
+    queUser: `${baseUrl}/hqbi/queuser`, // 用户答题记录、post方法
+    wsUrl: `wss://dati.beyond-itservice.com:8080/answer-api/websocket`,
+    roomSave: `${baseUrl}/hqbi/room`, // 添加房间、post方法
+  },
+};
--- pages/pk-home/pk-home.js
+++ pages/pk-home/pk-home.js
@@ -1,42 +1,25 @@
-// pages/home/home.js
+// pages/pk-home/pk-home.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:{
-      picture:'https://free.liaoningdoupo.com/files/logo2.png',
-      name:'小赵',
-      rank:'青铜',
-      score:'50'
-    },
-    time:'04-28 00:00:00',
-    list:[
-    {
-      title:'签到',
-      icon:'https://free.liaoningdoupo.com/files/sign.png',
-    },
-    {
-      title:'党务知识学习',
-      icon:'https://free.liaoningdoupo.com/files/xx.png',
-    },
-    {
-      title:'时代楷模排位',
-      icon:'https://free.liaoningdoupo.com/files/pw.png',
-    },
-    {
-      title:'党建先锋榜',
-      icon:'https://free.liaoningdoupo.com/files/bang.png',
-    },
-    {
-      title:'预赛赛制公告',
-      icon:'https://free.liaoningdoupo.com/files/gg.png',
-    },
-    {
-      title:'设置',
-      icon:'https://free.liaoningdoupo.com/files/sz.png',
-    },
+    userInfo:[
+      {
+        picture:'https://free.liaoningdoupo.com/files/logo2.png',
+        name:'小商',
+        leveal:'等级1',
+        dw:'黑铁',
+        deptName:'春熙路分行'
+      },
+      {
+        picture:'https://free.liaoningdoupo.com/files/logo2.png',
+        name:'小商2',
+        leveal:'等级1',
+        dw:'段位 黑铁',
+        deptName:'春熙路分行'
+      }
     ]
   },
 
